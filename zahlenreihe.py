554d312f687a6dedaf5fe3a8a328fe4256cfe3f6
samiandreas8 <171witb11@o365.htl-leoben.at>
2018-04-18 11:33:03 +0200

    asd

--- zahlenreihe.py
+++ zahlenreihe.py
@@ -1,4 +1,15 @@
 a=int(input("geben sie eine Zahl ein"))
 for i in range(1,a+1):
-    print(i, end=" ,")
-for i in range(1,a+1&&a%0):
+    if (i < n):
+        print(i, end=" ,")
+    else:
+        print(i)
+for i in range(1,a+1):
+    if (n%0):
+        if (i < n):
+            print(i, end=" ,")
+        else:
+            print(i)
+    
+        
+    
