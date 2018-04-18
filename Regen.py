41c440dbece913b451ea6e0f4ce8770ac913cb78
samiandreas8 <171witb11@o365.htl-leoben.at>
2018-04-18 10:33:28 +0200

    Vervollständigung Regen

--- Regen.py
+++ Regen.py
@@ -1,10 +1,12 @@
-Regen=True
-while(regen):
-    print("warten bis der Regen aufhört")
-    Regen1=boolean(input("Regnet es noch immer?"))
-    if(Regen1=="True"):
-        #nothing
-    else:
-        regen = False
-print("jetzt")
+regen=True
+if (regen==True):
+    Regenschirm=input("Hast du einen Regenschirm?y/n\n")
+    if (Regenschirm=="n"):
+        while(regen):
+            print("warten bis der Regen aufhört")
+            regen1=input("Regnet es noch immer?y/n\n")
+            if(regen1=="n"):
+                regen=False
+        print("jetzt regnet es nicht mehr")
+print ("Geh nach draußen")
 
