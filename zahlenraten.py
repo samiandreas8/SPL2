ab87b130c78ee28cbab3a19f3c182abadbba0a99
samiandreas8 <171witb11@o365.htl-leoben.at>
2018-04-18 12:21:17 +0200

    Zahlenrattt finish

--- zahlenraten.py
+++ zahlenraten.py
@@ -1,16 +1,17 @@
 import random
-n = in(input("Wählen sie den Zahlenruam aus"))
-zufallszahl = randint(1,n)
-m = int(input("Geben sie die Anzahl der Versuche ein"))
+n = int(input("Wählen sie den Zahlenruam aus\n"))
+zufallszahl = random.randint(1,n)
+m = int(input("Geben sie die Anzahl der Versuche ein\n"))
+i=1
 while(i<=m):
-    x=int(input("Geben sie ihren Tipp ein"))
+    x=int(input("Geben sie ihren Tipp ein\n"))
     if (x == zufallszahl):
         print("Sie haben richtig geraten!!")
-        break
+        i=m
     else:
-        print ("Leider Falsch sie haben noch", i, "Versuche")
+        print ("Leider Falsch sie haben noch", m-i, "Versuche\n")
         if (x > zufallszahl):
-            print("zu groß")
+            print("zu groß\n")
         else:
-            print("zu groß")
-continue
+            print("zu klein\n")
+    i+=1
