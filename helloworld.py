b8b6e95ac81c134f7ac1313536959ae86cb645a9
samiandreas8 <171witb11@o365.htl-leoben.at>
2018-04-18 10:05:19 +0200

    Erweiterung

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,10 @@
 print("Hallo Servus")
 name=input("Wie lautet ihr Name?\n")
 print("Schhön dich zu treffen", name, "Die länge von dem Namen ist", len(name),"Zeichen lang.")
-alter=int(input("Wie alt bist du?\n"))
-print("Du wirst in einem Jahr", alter+1, "Jahre alt sein.")
+alter=input("Wie alt bist du?\n")
+alter=int(alter)
+if (alter > 100):
+    print ("Das ist wohl ein Scherz  oder? ;-)")
+else:
+    print("Du wirst in einem Jahr", alter+1, "Jahre alt sein.")
+print ("Juice Tschuiss")
