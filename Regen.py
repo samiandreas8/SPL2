0bfb3734089a2970b8e71e3c7121ff9afd2ed4a2
samiandreas8 <171witb11@o365.htl-leoben.at>
2018-04-18 10:15:50 +0200

    Regen erstellt

---
+++ Regen.py
@@ -0,0 +1,10 @@
+Regen=True
+while(regen):
+    print("warten bis der Regen aufhört")
+    Regen1=boolean(input("Regnet es noch immer?"))
+    if(Regen1=="True"):
+        #nothing
+    else:
+        regen = False
+print("jetzt")
+
